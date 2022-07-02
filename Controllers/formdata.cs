932afbe3af187b02c677f4bd25b72c28ad15e30a
kkpc <kkpc@KK>
2022-07-02 13:36:22 +0500

    "Form Quiz"

---
+++ Controllers/formdata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public form Setformdata(string fname, string lname, string email, int tel,string gen,
+		string department)
+	{
+		Setformdata objj = new Setformdata();
+		objj.fname = fname;
+        objj.lname = lname;
+		objj.email = email;
+		objj.tel = tel;
+		objj.gen = gen;
+		objj.department = department;
+
+		return (Setformdata)
+	}
+}
